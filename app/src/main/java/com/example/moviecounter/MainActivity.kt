bdcb0d47901afd4565f3039950ef9a6b65644a3b
Patrick Vizcarra <patrick.vizcarra@tecsup.edu.pe>
2026-09-08 16:01:07 -0500

    feat: add Checkbox component

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -8,17 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moviecounter.ui.theme.MovieCounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MovieCounterTheme {
                 CardExample()
@@ -29,17 +35,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CardExample(modifier: Modifier = Modifier) {
+
+    var checked by remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
+
         Card {
             Text(
                 text = "Movie Counter",
                 modifier = Modifier.padding(16.dp)
             )
         }
+
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it }
+        )
     }
 }
 
